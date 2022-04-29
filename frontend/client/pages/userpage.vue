85064d8c45cdc6a20f935f112bcdc18556c5c7be
avogt <avogt@student.42.fr>
2022-04-29 17:22:50 +0200

    memo.txt and cats

---
+++ frontend/client/pages/userpage.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-row>
+	<v-col class="text-center">
+	  <img
+		src="/v.png"
+		alt="Vuetify.js"
+		class="mb-5"
+	  >
+	  Hello, this is a userpage
+	</v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'UserPage'
+}
+</script>
