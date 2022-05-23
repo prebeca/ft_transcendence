c2affbfa9b23571d0157106710919e8d506be923
t-pierre <tpierre@student.42.fr>
2022-05-23 19:05:04 +0200

    lot of changes, responsive almost done

--- backend/server/src/game/game.gateway.ts
+++ backend/server/src/game/game.gateway.ts
@@ -1,8 +1,36 @@
 import { ContextType, Logger } from "@nestjs/common";
 import { ConnectedSocket, MessageBody, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer, WsResponse } from "@nestjs/websockets";
 import { Server, Socket } from "socket.io";
+import { GameService } from "./game.service";
+import GameI from "./gameI.interface";
 
-const padHeight = 68;
+const gameWidth = 640;
+const gameHeight = 480;
+const padWidth = 10;
+const padHeight = 100;
+const ballradius = 7;
+const padSpeed = 10;
+
+function initGame(game: GameI, data: any) {
+	game.canvasWidth = data.width / 2;
+	game.canvasHeight = data.height / 2;
+	game.ratiox = gameWidth / game.canvasWidth;
+	game.ratioy = gameHeight / game.canvasHeight;
+	game.pad1x = game.canvasWidth / 10 - padWidth;
+	game.pad1y = game.canvasHeight / 2 - game.pad1Height / 2;
+	game.pad2x = game.canvasWidth - game.canvasWidth / 10;
+	game.pad2y = game.canvasHeight / 2 - game.pad2Height / 2;
+	game.ballx = game.canvasWidth / 2;
+	game.bally = game.canvasHeight / 2;
+	game.pad1Width = padWidth;
+	game.pad1Height = padHeight;
+	game.pad1Speed = padSpeed;
+	game.pad2Width = padWidth;
+	game.pad2Height = padHeight;
+	game.pad2Speed = padSpeed;
+	game.ballr = ballradius;
+	console.log("dans init");
+}
 
 @WebSocketGateway(42042, {cors: {
 	origin: process.env.APPLICATION_REDIRECT_URI,
@@ -11,16 +39,10 @@ const padHeight = 68;
 })
 export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
+	game = {} as GameI;
+
 	@WebSocketServer() server: Server;
 	private logger: Logger = new Logger("gameGateway");
-
-	pad1y: number;
-	pad2y: number;
-	ballx: number;
-	bally: number;
-	canvasx: number;
-	canvasy: number;
-
 
 	afterInit(server: Server) {
 		this.logger.log("game socket init !");
@@ -28,7 +50,6 @@ export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
 	handleDisconnect(client: Socket) {
 		console.log(`Client disconnected: ${client.id}`);
-
 	}
 
 	handleConnection(client: Socket, ...args: any[]) {
@@ -36,52 +57,66 @@ export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect {
 		console.log('Client connected username:		' + client.handshake.query.username);
 	}
 
-	// @SubscribeMessage('setUpPads')
-	// setUpPads(width: number, height: number) {
-	// 	// pad1.x = width;
-	// 	// pad1.y = height / 10;
-	// 	// pad1.width = width / 6,
-	// 	// pad1.height = height / 4,
-	// 	this.server.emit("pad1", pad1);
-	// 	this.server.emit("pad2", pad2);
-	// }
-
-	@SubscribeMessage('initPads')
-	initPads(client: Socket, data: any) {
-		this.canvasx = data.width;
-		this.canvasy = data.height;
-		this.pad1y = data.height / 2 - padHeight / 2;
-		this.pad2y = data.height / 2 - padHeight / 2;
-		this.ballx = data.width / 2;
-		this.bally = data.height / 2;
-		this.server.emit("drawPad", 1, this.pad1y);
-		this.server.emit("drawPad", 2, this.pad2y);
-		this.server.emit("drawBall", this.ballx, this.bally);
+	@SubscribeMessage('initGame')
+	initGame(client: Socket, data: any) {
+		initGame(this.game, data);
+		this.server.emit("initGame", this.game);
 	}
 
-	@SubscribeMessage('ArrowUp')
-	// padUp(client: Socket, pos: number) {
+	@SubscribeMessage('updateDimensions')
+	updateDimensions(client: Socket, data: any) {
+		let previousW = this.game.canvasWidth;
+		let previousH = this.game.canvasHeight;
+
+		this.game.canvasWidth = data.width / 2;
+		this.game.canvasHeight = data.height / 2;
+
+		const ratx = this.game.canvasWidth / previousW;
+		const raty = this.game.canvasHeight / previousH;
+
+		// this.game.ratiox *= gameWidth / this.game.canvasWidth;
+		// this.game.ratioy *= gameHeight / this.game.canvasHeight;
+		// const ratx = this.game.ratiox;
+		// const raty = this.game.ratioy;
+
+		this.game.pad1x *= ratx;
+		this.game.pad1y *= raty;
+		this.game.pad1Width *= ratx;
+		this.game.pad1Height *= raty;
+		this.game.pad1Speed *= raty;
+		this.game.pad2x *= ratx;
+		this.game.pad2y *= raty;
+		this.game.pad2Width *= ratx;
+		this.game.pad2Height *= raty;
+		this.game.pad2Speed *= raty;
+		this.game.ballx *= ratx;
+		this.game.bally *= raty;
+		console.log("speed = " + this.game.pad1Speed);
+		this.server.emit("updateDimensions", this.game);
+	}
+
+	@SubscribeMessage('arrowUp')
 	padUp(client: Socket) {
-		// console.log("pad1y avant = "+this.pad1y);
-		if (this.pad1y > 10)
-			this.pad1y -= 10;
+		if (this.game.pad1y > padSpeed)
+			this.game.pad1y -= padSpeed;
 		else
-			this.pad1y = 0;
-		// console.log("pad1y apres = "+this.pad1y);
-		this.server.emit("drawPad", 1, this.pad1y);
+		this.game.pad1y = 0;
+		this.server.emit("updatePad", this.game.pad1y);
 	}
 
-	@SubscribeMessage('ArrowDown')
-	// padUp(client: Socket, pos: number) {
+	@SubscribeMessage('arrowDown')
 	padDown(client: Socket) {
-		// console.log("pad1y avant = "+this.pad1y);
-		if (this.pad1y < this.canvasy - padHeight)
-			this.pad1y += 10;
+		if (this.game.pad1y < this.game.canvasHeight - this.game.pad1Height - padSpeed)
+			this.game.pad1y += padSpeed;
 		else
-			this.pad1y = this.canvasy - padHeight;
-		// console.log("pad1y apres = "+this.pad1y);
-		this.server.emit("drawPad", 1, this.pad1y);
+			this.game.pad1y = this.game.canvasHeight - this.game.pad1Height;
+		this.server.emit("updatePad", this.game.pad1y);
 	}
+
+
+
+
+
 
 	// @SubscribeMessage('msgToServer')
 	// handleMessage(client: Socket, message: string): void {
--- frontend/client/pages/game.vue
+++ frontend/client/pages/game.vue
@@ -1,9 +1,7 @@
 <template>
-  <div>
-    <div id="myGame">
-      <canvas id="canvas"></canvas>
-    </div>
-  </div>
+	<div id="myGame">
+	  <canvas id="canvas"></canvas>
+	</div>
 </template>
 
 <script lang="ts">
@@ -16,131 +14,181 @@ import GameI from "../types/interfaces/gameI.interface"
 export default Vue.extend({
   name: "Game",
   data() {
-    return {
-      socket: io(),
-      canvas: {} as HTMLCanvasElement,
-      context: {} as CanvasRenderingContext2D | null,
-      // game: {} as GameI,
-      pad1: {} as PadI,
-      pad2: {} as PadI,
-      ball: {} as BallI,
-    };
+	return {
+	  socket: io(),
+	  canvas: {} as HTMLCanvasElement,
+	  context: {} as CanvasRenderingContext2D | null,
+	  game: {} as GameI,
+	  pad1: {} as PadI,
+	  pad2: {} as PadI,
+	  ball: {} as BallI,
+	};
   },
   created() {
-    this.socket = io(process.env.API_SOCKET_GAME);
-    this.pad1.width = 10;
-    this.pad1.height = 68;
-    this.pad2.width = 10;
-    this.pad2.height = 68;
+	this.socket = io(process.env.API_SOCKET_GAME);
   },
   beforeMount() {
-    this.socket.on("print", (data) => {
-      this.myPrint(data);
-    });
+	this.socket.on("print", (data) => {
+	  this.myPrint(data);
+	});
 
-    this.socket.on("drawPad", (nb: number, pady: number) => {
-      this.drawPad(nb, pady);
-    });
-    this.socket.on("drawBall", (ballx: number, bally: number) => {
-      console.log("dans socket " + ballx + "----" + bally)
-      this.drawBall(ballx, bally);
-    });
+	this.socket.on("initGame", (data: GameI) => {
+	  this.initGame(data);
+	});
+
+	this.socket.on("updatePad", (data: number) => {
+		this.game.pad1y = data;
+		this.pad1.y = this.game.pad1y;
+		this.drawPad(this.pad1);
+	});
+	this.socket.on("drawBall", (ballx: number, bally: number) => {
+	  console.log("dans socket " + ballx + "----" + bally)
+	  this.drawBall(ballx, bally);
+	});
+	this.socket.on("updateDimensions", (data: GameI) => {
+	  this.initGame(data);
+	});
   },
   mounted() {
-    this.setUpCanvas();
-    this.play();
+	this.canvas = document.getElementById("canvas");
+	if (!this.canvas) {
+	  return;
+	}
+	this.context = this.canvas.getContext("2d");
+	this.socket.emit('initGame', {width: window.innerWidth, height: window.innerHeight});
 
-    window.addEventListener("keydown", (event) => {
-      if (event.key === "ArrowUp") {
-        // this.socket.emit("ArrowUp", this.pad1.y);
-        this.socket.emit("ArrowUp");
-      }
-      if (event.key === "ArrowDown") {
-        // this.socket.emit("ArrowUp", this.pad1.y);
-        this.socket.emit("ArrowDown");
-      }
-    });
+	this.play();
+
+	window.addEventListener("keydown", (event) => {
+	  if (event.key === "ArrowUp") {
+		this.socket.emit("arrowUp");
+	  }
+	  if (event.key === "ArrowDown") {
+		this.socket.emit("arrowDown");
+	  }
+	});
+
+	const handleResize = (event) => {
+		this.socket.emit('updateDimensions', {width: window.innerWidth, height: window.innerHeight});
+	};
+
+	window.addEventListener('resize', handleResize);
+
   },
   methods: {
-    setUpCanvas() {
-      this.canvas = document.getElementById("canvas");
-      if (!this.canvas) {
-        return;
-      }
-      this.context = this.canvas.getContext("2d");
-      this.canvas.width = window.innerWidth / 2;
-      this.canvas.height = window.innerHeight / 2;
-      this.pad1.x = 0;
-      this.pad2.x = this.canvas.width - this.pad1.width;
-      this.ball.r = 6;
-      this.drawCanvas();
-      this.socket.emit("initPads", {width: this.canvas.width, height: this.canvas.height});
-    },
-    play() {
-      this.drawBall(this.ball.x + 1, this.ball.y + 1);
-      requestAnimationFrame(this.play);
-    },
-    draw() {
-      this.drawCanvas();
-    },
-    drawCanvas() {
-      this.context.fillStyle = "black";
-      this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
-      this.context.strokeStyle = "white";
-      this.context.beginPath();
-      this.context.moveTo(this.canvas.width / 2, 0);
-      this.context.lineTo(this.canvas.width / 2, this.canvas.height);
-      this.context.stroke();
-    },
-    drawPad(nb: number, pady: number) {
-      let pad: PadI;
-      if (nb === 1)
-        pad = this.pad1;
-      else pad = this.pad2;
-      this.erasePad(pad, pady);
-      pad.y = pady;
-      this.context.fillStyle = "white";
-      this.context.fillRect(pad.x, pad.y, pad.width, pad.height);
-    },
-    erasePad(pad: PadI, pady: number) {
-      // this.context.clearRect(pad.x, pad.y - 1, pad.width, pad.height + 1);
-      this.context.fillStyle = "black";
-      this.context.fillRect(pad.x, pad.y - 1, pad.width, pad.height + 2);
-    },
-    drawBall(ballx: number, bally: number) {
-      this.eraseBall(this.ball);
-      this.context.beginPath();
-      this.ball.x = ballx;
-      this.ball.y = bally;
-      this.context.fillStyle = "white";
-      this.context.arc(
-        this.ball.x,
-        this.ball.y,
-        this.ball.r,
-        0,
-        Math.PI * 2,
-        false
-      );
-      this.context.fill();
-    },
-    eraseBall(ball) {
-      this.context.beginPath();
-      this.context.fillStyle = "black";
-      this.context.arc(
-        this.ball.x,
-        this.ball.y,
-        this.ball.r + 1,
-        0,
-        Math.PI * 2,
-        false
-      );
-      this.context.fill();
-      // this.context.clearRect(ball.x, ball.y, ball.r);
-      // this.drawCanvas();
-    },
-    myPrint(data) {
-      console.log(data);
-    },
+	initGame(data: GameI) {
+		this.game = data;
+		this.canvas.width = data.canvasWidth;
+		this.canvas.height = data.canvasHeight;
+		this.pad1.width = this.game.pad1Width;
+		this.pad1.height = this.game.pad1Height;
+		this.pad1.speed = this.game.pad1Speed;
+		this.pad2.width = this.game.pad2Width;
+		this.pad2.height = this.game.pad2Height;
+		this.pad2.speed = this.game.pad2Speed;
+		this.pad1.x = this.game.pad1x;
+		this.pad2.x = this.game.pad2x;
+		// this.pad2.x = this.canvas.width - this.pad1.width;
+		this.pad1.y = this.game.pad1y;
+		this.pad2.y = this.game.pad2y;
+		this.ball.x = this.game.ballx;
+		this.ball.y = this.game.bally;
+		this.ball.r = this.game.ballr;
+		this.draw();
+	},
+	play() {
+	//   this.drawBall(this.ball.x + 1, this.ball.y + 1);
+		this.draw();
+		requestAnimationFrame(this.play);
+	},
+	draw() {
+		this.drawCanvas();
+		this.drawPad(this.pad1);
+		this.drawPad(this.pad2);
+		this.drawBall();
+		console.log("dans draw");
+	},
+	drawCanvas() {
+	  this.context.fillStyle = "black";
+	  this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+	  this.context.strokeStyle = "white";
+	  this.context.beginPath();
+	  this.context.moveTo(this.canvas.width / 2, 0);
+	  this.context.lineTo(this.canvas.width / 2, this.canvas.height);
+	  this.context.stroke();
+	},
+	drawPad(pad: PadI) {
+		// this.erasePad(pad);
+		this.context.fillStyle = "white";
+		this.context.fillRect(pad.x, pad.y, pad.width, pad.height);
+	},
+	// erasePad(pad: PadI) {
+	// 	this.context.fillRect(pad.x, pad.y - 1, pad.width, pad.height + 2);
+	// },
+	// drawPad(nb: number, pady: number) {
+	//   let pad: PadI;
+	//   if (nb === 1)
+	// 	pad = this.pad1;
+	//   else pad = this.pad2;
+	//   this.erasePad(pad, pady);
+	//   pad.y = pady;
+	//   this.context.fillStyle = "white";
+	//   this.context.fillRect(pad.x, pad.y, pad.width, pad.height);
+	// },
+	// erasePad(pad: PadI, pady: number) {
+	//   // this.context.clearRect(pad.x, pad.y - 1, pad.width, pad.height + 1);
+	//   this.context.fillStyle = "black";
+	//   this.context.fillRect(pad.x, pad.y - 1, pad.width, pad.height + 2);
+	// },
+
+	drawBall() {
+	  this.context.beginPath();
+	  this.context.fillStyle = "white";
+	  this.context.arc(
+		this.ball.x,
+		this.ball.y,
+		this.ball.r,
+		0,
+		Math.PI * 2,
+		false
+	  );
+	  this.context.fill();
+	},
+	// drawBall(ballx: number, bally: number) {
+	//   this.eraseBall(this.ball);
+	//   this.context.beginPath();
+	//   this.ball.x = ballx;
+	//   this.ball.y = bally;
+	//   this.context.fillStyle = "white";
+	//   this.context.arc(
+	// 	this.ball.x,
+	// 	this.ball.y,
+	// 	this.ball.r,
+	// 	0,
+	// 	Math.PI * 2,
+	// 	false
+	//   );
+	//   this.context.fill();
+	// },
+	// eraseBall(ball: BallI) {
+	//   this.context.beginPath();
+	//   this.context.fillStyle = "black";
+	//   this.context.arc(
+	// 	this.ball.x,
+	// 	this.ball.y,
+	// 	this.ball.r + 1,
+	// 	0,
+	// 	Math.PI * 2,
+	// 	false
+	//   );
+	//   this.context.fill();
+	//   // this.context.clearRect(ball.x, ball.y, ball.r);
+	//   // this.drawCanvas();
+	// },
+
+	myPrint(data) {
+	  console.log(data);
+	},
   },
 });
 </script>
@@ -175,33 +223,33 @@ import socket from 'socket.io'
 import io from 'socket.io-client'
 
 export default Vue.extend({
-    data() {
-        return {
-            title: 'Websockets Tester',
-            text: '',
-            messages: ['Some message', 'Another message'],
-            socket: null,
-        }
-    },
-    methods: {
-        sendMessage() {
-            // this.messages.push(this.text);
-            console.log(`send: ${this.text}`);
-            this.socket.emit('msgToServer', this.text);
-            this.text = '';
-        },
-        receiveMessage(msg) {
-            console.log(`recv: ${msg}`);
-            this.messages.push(msg);
-            // this.text = '';
-        },
-    },
-    created() {
-        this.socket = io(process.env.API_SOCKET_GAME);
-        this.socket.on('msgToClient', (msg) => {
-            this.receiveMessage(msg);
-        });
-    }
+	data() {
+		return {
+			title: 'Websockets Tester',
+			text: '',
+			messages: ['Some message', 'Another message'],
+			socket: null,
+		}
+	},
+	methods: {
+		sendMessage() {
+			// this.messages.push(this.text);
+			console.log(`send: ${this.text}`);
+			this.socket.emit('msgToServer', this.text);
+			this.text = '';
+		},
+		receiveMessage(msg) {
+			console.log(`recv: ${msg}`);
+			this.messages.push(msg);
+			// this.text = '';
+		},
+	},
+	created() {
+		this.socket = io(process.env.API_SOCKET_GAME);
+		this.socket.on('msgToClient', (msg) => {
+			this.receiveMessage(msg);
+		});
+	}
 })
 
 
