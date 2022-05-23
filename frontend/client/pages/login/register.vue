6f6c1716a666fd841b6e085e44ffa25c15544eac
Amelie FOULQUIER <afoulqui@e2r10p3.clusters.42paris.fr>
2022-05-23 13:15:49 +0200

    cleaned files + created new routes

---
+++ frontend/client/pages/login/register.vue
@@ -0,0 +1,16 @@
+<template>
+  <div
+    style="height: 100vh; max-height: 100%"
+    class="d-flex flex-column justify-center align-center"
+  >
+    <LoginEmailForm />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default {
+  layout: "empty",
+};
+</script>
---
+++ frontend/client/pages/user/edit.vue
@@ -0,0 +1,13 @@
+<template>
+  <div
+    style="height: 80vh; max-height: 100%"
+    class="d-flex flex-column justify-center align-center"
+  >
+    <UserEditProfilCard />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({});
+</script>
