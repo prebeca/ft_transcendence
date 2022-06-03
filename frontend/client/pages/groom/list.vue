5b80206fa9742b3541972fe10364cc6de7f5fbd1
avogt <avogt@student.42.fr>
2022-06-03 15:32:51 +0200

    eeee

--- frontend/client/pages/groom/list.vue
+++ frontend/client/pages/groom/list.vue
@@ -25,6 +25,7 @@ import Vue from "vue";
 export default Vue.extend({
   data() {
     return {
+      intervalID: {} as NodeJS.Timer,
       rooms: [
         {
           roomname: "",
@@ -35,31 +36,40 @@ export default Vue.extend({
     };
   },
   created: async function () {
-    await this.$axios
-      .get("/gameroom/list")
-      .then((res) => {
-        console.log(res.data);
-        var array: { roomname: string; player1: string; player2: string }[];
-        array = res.data;
-        for (let i = 0; i < array.length; i++) {
-          if (i === 0 && this.rooms[0].roomname === "") {
-            this.rooms[0].roomname = array[i].roomname;
-            this.rooms[0].player1 = array[i].player1;
-            this.rooms[0].player2 = array[i].player2;
-          } else {
-            this.rooms.push({
-              roomname: array[i].roomname,
-              player1: array[i].player1,
-              player2: array[i].player2,
-            });
-          }
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    await this.getList();
+    this.intervalID = setInterval(this.getList, 1000);
+  },
+  beforeDestroy() {
+    clearInterval(this.intervalID);
   },
   methods: {
+    async getList() {
+      var new_rooms = [{ roomname: "", player1: "", player2: "" }];
+      await this.$axios
+        .get("/gameroom/list")
+        .then((res) => {
+          console.log(res.data);
+          var array: { roomname: string; player1: string; player2: string }[];
+          array = res.data;
+          for (let i = 0; i < array.length; i++) {
+            if (i === 0 && new_rooms[0].roomname === "") {
+              new_rooms[0].roomname = array[i].roomname;
+              new_rooms[0].player1 = array[i].player1;
+              new_rooms[0].player2 = array[i].player2;
+            } else {
+              new_rooms.push({
+                roomname: array[i].roomname,
+                player1: array[i].player1,
+                player2: array[i].player2,
+              });
+            }
+          }
+          this.rooms = new_rooms;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
     async join(name: string) {
       if (name === "") return;
       this.$router.push({ path: "/groom/room", query: { name: name } });
