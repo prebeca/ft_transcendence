f1604f28b4db12837a51a0412acabdcbc7473184
Pierre REBECA <prebeca@e2r1p13.clusters.42paris.fr>
2022-08-05 21:42:05 +0200

    remove log

--- backend/server/src/chat/socket/socket.service.ts
+++ backend/server/src/chat/socket/socket.service.ts
@@ -54,7 +54,6 @@ export class SocketService {
 	}
 
 	async newMessage(user: User, message: Message, server: Server) {
-		console.log(user.username + ": " + message.content);
 		let channel = await this.channelService.findOneById(message.channel.id);
 
 		message.channel.scope = "channel.scope"
